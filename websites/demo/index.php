20da20746471165eaddd34b94afff2ba8db7986b
Gabriel <lumos2719@gmail.com>
2026-07-09 23:48:10 -0500

    array de arrays y como imprimirlos, son como diccionarios que imprimen el valor mediente el key, asociar el link al nombre e imprimirlo

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,29 +3,50 @@
 <head>
     <meta charset="UTF-8">
 
-    <title>Demo</title>
+    <title>Gabriel</title>
 </head>
 <body>
-    <h1>Recommended Books</h1>
+    <h1>My Best Heros</h1>
 
     <?php
         $books = [
-                "Pantaleon y las visitadoras",
-                "Rango",
-                "Illia Topuria"
+                [
+                        'name' =>'Phanton lancer',
+                        'Game style' => 'Late game, doge spells',
+                        'Player' => 'Ana',
+                        'profileUrl' => "http://example.com"
+                ],
+                [
+                        'name' => 'Whrait King',
+                        'Game style' => 'Snow Ball',
+                        'player' => 'k1',
+                        'profileUrl' => "http://example.com"
+                ],
+                [
+                        'name' => 'Spectre',
+                        'Game style' => 'Late game, tanky',
+                        'player' => 'Humble Good',
+                        'profileUrl' => "http://example.com"
+                ],
         ]
     ?>
-    <ul>
-        <?php foreach ($books as $book){
-            echo "<li>$book</li>";
-        } ?>
-    </ul>
 
-    <ul>
-        <?php foreach ($books as $book):?>
-            <li><?=$book?></li>
-        <?php endforeach ?>
-    </ul>
+    <p>
+        <?php foreach ($books as $book): ?>
+            <li><?= $book['name'] ?></li>
+        <?php endforeach; ?>
+    </p>
+
+    <p>
+        <?php foreach ($books as $book): ?>
+            <li>
+                <a href="<?= $book['profileUrl'] ?>">
+                    <?= $book['name'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </p>
+
 
 </body>
 </html>
